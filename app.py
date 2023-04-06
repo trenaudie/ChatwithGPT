c1e51ea149cefd7fe84b53700c7eb8137402356c
JarrryGuillaume <guillaume.jarry5@gmail.com>
2023-04-06 11:00:01 +0200

    Frontend update

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['document']
+    if uploaded_file:
+        # Save the file temporarily
+        filename = secure_filename(uploaded_file.filename)
+        filepath = os.path.join('temp', filename)
+        uploaded_file.save(filepath)
+
+        # Process the file and save it to the database
+        # You will need to implement this part based on the type of database you are using
+        save_file_to_database(filepath)
+
+        # Remove the temporary file
+        os.remove(filepath)
+
+        return 'File uploaded and saved to the database.', 200
+    else:
+        return 'No file was uploaded.', 400
+
+
+def save_file_to_database(filepath):
+    return False
+
+
+if __name__ == '__main__':
+    app.run()
