e492b69094da300f22bcd06ada03c32476ae0973
JarrryGuillaume <guillaume.jarry5@gmail.com>
2023-04-07 16:20:49 +0200

    ok

---
+++ static/webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+};
